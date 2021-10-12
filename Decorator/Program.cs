bb96bea7a0955b80f2a384d43030b000e710d2e5
Tural Novruzov <TuraliNovruzov@outlook.com>
2021-10-12 21:36:38 +0400

    Add Decorator, Facade Patterns

---
+++ Decorator/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+
+namespace Decorator
+{
+    #region Renderer
+    //abstract class AbstractRenderer
+    //{
+    //    public abstract void RenderInformation(string text);
+    //}
+
+
+    //class Renderer : AbstractRenderer
+    //{
+    //    public override void RenderInformation(string text)
+    //    {
+    //        Console.WriteLine(text);
+    //    }
+    //}
+
+
+
+    //class BaseDecorator : AbstractRenderer
+    //{
+    //    protected AbstractRenderer _renderer { get; set; }
+
+    //    public BaseDecorator(AbstractRenderer renderer)
+    //    {
+    //        _renderer = renderer;
+    //    }
+
+    //    public override void RenderInformation(string text)
+    //    {
+    //        _renderer.RenderInformation(text);
+    //    }
+    //}
+
+
+    //class ColorDecorator : BaseDecorator
+    //{
+    //    private ConsoleColor _color { get; set; }
+
+    //    public ColorDecorator(AbstractRenderer renderer, ConsoleColor color) : base(renderer)
+    //    {
+    //        _color = color;
+    //    }
+
+
+    //    public override void RenderInformation(string text)
+    //    {
+    //        var oldColor = Console.ForegroundColor;
+    //        Console.ForegroundColor = _color;
+    //        _renderer.RenderInformation(text);
+    //        Console.ForegroundColor = oldColor;
+    //    }
+    //}
+
+
+    //class UnderlineDecorator : BaseDecorator
+    //{
+    //    public UnderlineDecorator(AbstractRenderer renderer):base(renderer){}
+
+    //    public override void RenderInformation(string text)
+    //    {
+    //        _renderer.RenderInformation(text);
+
+    //        foreach (var item in text)
+    //        {
+    //            Console.Write("-");
+    //        }
+
+    //        Console.WriteLine();
+    //    }
+    //}
+
+
+
+    //class UpperCaseDecorator : BaseDecorator
+    //{
+    //    public UpperCaseDecorator(AbstractRenderer renderer):base(renderer){}
+
+    //    public override void RenderInformation(string text)
+    //    {
+    //        _renderer.RenderInformation(text.ToUpper());
+    //    }
+    //}
+
+
+
+
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        AbstractRenderer renderer = new Renderer();
+    //        renderer.RenderInformation("Salam");
+
+    //        renderer = new ColorDecorator(renderer, ConsoleColor.Red);
+    //        renderer.RenderInformation("Salam");
+
+    //        //renderer = new UnderlineDecorator(renderer);
+    //        //renderer.RenderInformation("Salam");
+
+    //        renderer = new UpperCaseDecorator(renderer);
+    //        renderer.RenderInformation("Salam");
+
+    //    }
+    //}
+
+    #endregion
+
+
+    #region Notifier
+    interface INotifier
+    {
+        public void Send(string message);
+    }
+
+
+    class Notifier : INotifier
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Message sent with Email"); ;
+        }
+    }
+
+    abstract class BaseDecorator : INotifier
+    {
+        protected INotifier _notifier { get; set; }
+
+        public BaseDecorator(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        public virtual void Send(string message)
+        {
+            _notifier.Send(message);
+        }
+    }
+
+
+    class TelegramDecorator : BaseDecorator
+    {
+        public TelegramDecorator(INotifier notifier):base(notifier){}
+        
+        public override void Send(string message)
+        {
+            // hard code
+            base.Send(message);
+            Console.WriteLine("Message sent with Telegram");
+        }
+    }
+
+
+    class FacebookDecorator : BaseDecorator
+    {
+        public FacebookDecorator(INotifier notifier) : base(notifier) { }
+
+        public override void Send(string message)
+        {
+            // hard code
+            base.Send(message);
+            Console.WriteLine("Message sent with Facebook");
+        }
+    }
+
+
+    class SlackDecorator : BaseDecorator
+    {
+        public SlackDecorator(INotifier notifier) : base(notifier) { }
+
+        public override void Send(string message)
+        {
+            // hard code
+            base.Send(message);
+            Console.WriteLine("Message sent with Slack");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INotifier notifier = new Notifier();
+
+            notifier = new TelegramDecorator(notifier);
+            notifier = new FacebookDecorator(notifier);
+            notifier = new SlackDecorator(notifier);
+
+            notifier.Send("Discount 50%");
+        }
+    }
+    #endregion
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Facade
+{
+    interface IDevice
+    {
+        string Vendor { get; set; }
+        string Model { get; set; }
+
+        void Start();
+    }
+
+    class CPU : IDevice
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("CPU started");
+        }
+    }
+
+
+    class RAM : IDevice
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("RAM started");
+        }
+    }
+
+    class GPU : IDevice
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("GPU started");
+        }
+    }
+
+    class MotherBoard : IDevice
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Motherboard started");
+        }
+
+    }
+
+    class PowerSupply : IDevice
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("PowerSupply started");
+        }
+    }
+
+    class Case : IDevice
+    {
+        public List<IDevice> Devices { get; set; } = new();
+
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+
+        public void AddDevice(IDevice _otherdevice)
+        {
+            Devices.Add(_otherdevice);
+        }
+
+        public void Start()
+        {
+            Devices.ForEach(e => e.Start());
+            Console.WriteLine("Computer started");
+        }
+    }
+
+
+    class ComputerFacade
+    {
+        MotherBoard mb = new MotherBoard();
+        RAM ram = new RAM();
+        GPU gpu = new GPU();
+        CPU cpu = new CPU();
+        PowerSupply ps = new PowerSupply();
+        Case c = new Case();
+
+        public void Start()
+        {
+            // some hard code 
+
+            c.AddDevice(mb);
+            c.AddDevice(ram);
+            c.AddDevice(gpu);
+            c.AddDevice(cpu);
+            c.AddDevice(ps);
+
+            c.Start();
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerFacade facade = new ComputerFacade();
+            facade.Start();
+        }
+    }
+}
