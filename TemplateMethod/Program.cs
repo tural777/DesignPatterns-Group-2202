5548a3404407ad1542f68de1b369ea1bfd89ee80
Tural Novruzov <t.novruzov@itstep.edu.az>
2021-10-18 18:16:11 +0400

    Add TemplateMethod and Visitor Design Patterns

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+
+    abstract class SQLEngine
+    {
+        public virtual void TemplateMethod()
+        {
+            Connect();
+            ExecuteCommand();
+            CloseConnection();
+        }
+
+        public abstract void Connect();
+        public abstract void ExecuteCommand();
+        public abstract void CloseConnection();
+    }
+
+
+    class MsSql : SQLEngine
+    {
+        public override void CloseConnection()
+        {
+            Console.WriteLine("MsSql CloseConnection");
+        }
+
+        public override void Connect()
+        {
+            Console.WriteLine("MsSql Connect");
+        }
+
+        public override void ExecuteCommand()
+        {
+            Console.WriteLine("MsSql ExecuteCommand");
+        }
+    }
+
+    class MySql : SQLEngine
+    {
+        public override void CloseConnection()
+        {
+            Console.WriteLine("MySql CloseConnection");
+        }
+
+        public override void Connect()
+        {
+            Console.WriteLine("MySql Connect");
+        }
+
+        public override void ExecuteCommand()
+        {
+            Console.WriteLine("MySql ExecuteCommand");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MsSql msSql = new MsSql();
+            msSql.TemplateMethod();
+
+            MySql mySql = new MySql();
+            mySql.TemplateMethod();
+        }
+    }
+}
---
+++ Visitor/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Visitor
+{
+    class MobilePhone
+    {
+        public string Model { get; set; }
+
+        public MobilePhone(string model)
+        {
+            Model = model;
+        }
+
+        // public void TakeAPhoto(){}
+
+        public void Accept(IMobileVisitor visitor) => visitor.Visit(this);
+    }
+
+    class IphoneX : MobilePhone
+    {
+        public IphoneX() : base("IphoneX") { }
+    }
+
+    class Mi8 : MobilePhone
+    {
+        public Mi8() : base("Mi8") { }
+    }
+
+    class Nokia3310 : MobilePhone
+    {
+        public Nokia3310() : base("Nokia3310") { }
+    }
+
+    interface IMobileVisitor
+    {
+        void Visit(MobilePhone mobilePhone);
+    }
+
+    class PhotoVisitor : IMobileVisitor
+    {
+        public void Visit(MobilePhone mobilePhone)
+        {
+            Console.WriteLine($"{mobilePhone.Model} telefonu ile shekil chekildi");
+        }
+    }
+
+    class SnakeGameVisitor : IMobileVisitor
+    {
+        public void Visit(MobilePhone mobilePhone)
+        {
+            Console.WriteLine($"{mobilePhone.Model} telefonu ile ilan oyunu oynanilir");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoVisitor photoVisitor = new();
+
+            IphoneX iphoneX = new();
+            iphoneX.Accept(photoVisitor);
+
+            Mi8 mi8 = new Mi8();
+            mi8.Accept(photoVisitor);
+
+
+
+            SnakeGameVisitor snakeGameVisitor = new();
+
+            Nokia3310 nokia3310 = new();
+            nokia3310.Accept(snakeGameVisitor);
+        }
+    }
+}
